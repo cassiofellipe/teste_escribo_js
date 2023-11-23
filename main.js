be2ff9cebc03bf50d1188c7e7d4ea8ead8ee66ff
Lucas Felipe <lucas.felipe@acoart.local>
2023-11-23 10:01:08 -0300

    Melhorando o COD.

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
-function sumNumbersMultiple() {
-  
-  let inputNumber = input("Favor digite um numero inteiro: ");
-  let result = 0;
+const readline = require('readline');
 
-  for (let i = 3; i < inputNumber; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      result += i;
+function solicitarNumero() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  let soma = 0;
+
+  rl.question('Digite um número: ', (numero) => {
+    for (let i = 3; i < numero; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        soma += i;
+      }
     }
-  }
 
-  return console.log(`O valor passado é: ${inputNumber}, o resultado esperado é: ${result}`);
+    console.log(`A soma dos múltiplos de 3 e 5 até ${numero} é: ${soma}`);
+    rl.close();
+  });
 }
 
-sumNumbersMultiple();
-
-// function imprimirValor() {
-//   // Obtém a entrada do usuário via prompt
-//   var userInput = prompt("Digite um valor:");
-
-//   // Converte a entrada para um número (se possível)
-//   var valor = parseFloat(userInput);
-
-//   // Verifica se a conversão foi bem-sucedida
-//   if (!isNaN(valor)) {
-//       // Imprime o valor
-//       console.log("O valor informado é: " + valor);
-//   } else {
-//       // Se a entrada não pôde ser convertida para um número
-//       console.log("Por favor, insira um valor numérico válido.");
-//   }
-// }
-
-// // Chama a função para imprimir o valor
-// imprimirValor();
+// Chame a função para testar
+solicitarNumero();
